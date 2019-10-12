3bf725bfcaa71e1cb2eb19c8110ee3e436ccb34e
KayXu <34438446+CorKay@users.noreply.github.com>
2019-10-12 15:48:27 +0800

    Update db_base.h

--- db_base.h
+++ db_base.h
@@ -10,9 +10,9 @@
 //底层数据库操作函数接口
 MYSQL* db_connect(const char* host, const char* user, const char* passwd, const char* db_name);
 MYSQL* db_get_connect(const char* host, const char* user, const char* passwd, const char* db_name);
-int db_disconnect(MYSQL* mysql);
-int db_get_disconnect(MYSQL* mysql);
+void db_disconnect(MYSQL* mysql);
+//int db_get_disconnect(MYSQL* mysql);
 int db_change_query(MYSQL* mysql, const char* query);
-int db_select_query(MYSQL* mysql, const char* query);
+MYSQL_ROW* db_select_query(MYSQL* mysql, const char* query);
 
 #endif // !_DB_BASE_H
