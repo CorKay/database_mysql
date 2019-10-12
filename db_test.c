255565776164c7516689565eae284d24806e17b7
KayXu <34438446+CorKay@users.noreply.github.com>
2019-10-12 15:54:32 +0800

    Update db_test.c

--- db_test.c
+++ db_test.c
@@ -14,17 +14,27 @@ int main()
 	char* prep_sel_query = "SELECT * from %s";
 	char select_query[256] = { 0 };
 
-	MYSQL* mysql = db_get_connect(host, user, passwd, db_name);					//连接
+	MYSQL* mysql = db_get_connect(host, user, passwd, db_name);						//连接
+	if (mysql == NULL)
+	{
+		printf("db_connect failed \n");
+		syslog(LOG_ERR, "db_connect failed\n");
+		return 0;
+	}
 
-	sprintf(insert_query, prep_ins_query, "names", "'Kay'");          
+	sprintf(insert_query, prep_ins_query, "names", "'WangMing'");						//拼接插入语句
 
-	sprintf(select_query, prep_sel_query, "test_table");		
+	sprintf(select_query, prep_sel_query, "test_table");							//拼接查询语句
 
 	db_change_query(mysql, insert_query);									//插入
 
 	db_select_query(mysql, select_query);									//查询
 
-	db_get_disconnect(mysql);												//断开连接
+	db_select_query(mysql, "UPDATE test_table set names = 'Susan' where user_id = 4");			//修改
+
+	db_select_query(mysql, select_query);									//查询
+
+	db_disconnect(mysql);											//断开连接
 
 	return 0;
 }
