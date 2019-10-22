af5fbbdcf0d306e047440325be3a2671019150cd
KayXu <34438446+CorKay@users.noreply.github.com>
2019-10-22 16:57:35 +0800

    Update db_test.c

--- db_test.c
+++ db_test.c
@@ -3,38 +3,44 @@
 //Test
 int main()
 {
-	const char* host = "localhost";
-	const char* user = "root";
-	const char* passwd = "123";
-	const char* db_name = "test";
+	test_info example = { 30,"XiaoHong" };
+	test_info new_example = { 30,"LaoHong" };
 
-	char* prep_ins_query = "INSERT INTO test_table (%s) VALUES (%s)";
-	char insert_query[512] = { 0 };
+	MYSQL_RES** test_ret;
 
-	char* prep_sel_query = "SELECT * from %s";
-	char select_query[256] = { 0 };
-
-	MYSQL* mysql = db_get_connect(host, user, passwd, db_name);						//连接
-	if (mysql == NULL)
+	
+	if (test_table_insert(&example) == 0)				//插入
 	{
-		printf("db_connect failed \n");
-		syslog(LOG_ERR, "db_connect failed\n");
-		return 0;
+		printf("Insert test successful \n");
 	}
-
-	sprintf(insert_query, prep_ins_query, "names", "'WangMing'");						//拼接插入语句
-
-	sprintf(select_query, prep_sel_query, "test_table");							//拼接查询语句
-
-	db_change_query(mysql, insert_query);									//插入
-
-	db_select_query(mysql, select_query);									//查询
-
-	db_change_query(mysql, "UPDATE test_table set names = 'Susan' where user_id = 4");			//修改
-
-	db_select_query(mysql, select_query);									//查询
-
-	db_disconnect(mysql);											//断开连接
-
+	if (test_table_select(test_ret) == 0)				//查询
+	{
+		printf("Select1 test successful \n");
+		print_res(*test_ret);							//测试返回内容
+		db_free_select_result(test_ret);
+	}
+	
+	if (test_table_update(&new_example) == 0)			//更新
+	{
+		printf("Update test successful \n");
+	}
+	if (test_table_select(test_ret) == 0)				//查询
+	{
+		printf("Select2 test successful \n");
+		print_res(*test_ret);
+		db_free_select_result(test_ret);
+	}
+	
+	if (test_table_delete(&new_example) == 0)			//删除
+	{
+		printf("Delete test successful \n");
+	}
+	if (test_table_select(test_ret) == 0)				//查询
+	{
+		printf("Select3 test successful \n");
+		print_res(*test_ret);
+		db_free_select_result(test_ret);
+	}
+	
 	return 0;
 }
