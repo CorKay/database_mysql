d0ac01b9aa448fa7317c63285c392d77c67c87bf
KayXu <34438446+CorKay@users.noreply.github.com>
2019-10-23 10:37:23 +0800

    Update db_test.c

--- db_test.c
+++ db_test.c
@@ -9,33 +9,24 @@ int main()
 	MYSQL_RES** test_ret;
 
 	
-	if (test_table_insert(&example) == 0)				//插入
-	{
-		printf("Insert test successful \n");
-	}
-	if (test_table_select(test_ret) == 0)				//查询
+	test_table_insert(&example);						//插入
+	if (test_table_select(test_ret) == 0)					//查询
 	{
 		printf("Select1 test successful \n");
-		print_res(*test_ret);							//测试返回内容
+		print_res(*test_ret);						//测试返回内容
 		db_free_select_result(test_ret);
 	}
 	
-	if (test_table_update(&new_example) == 0)			//更新
-	{
-		printf("Update test successful \n");
-	}
-	if (test_table_select(test_ret) == 0)				//查询
+	test_table_update(&new_example);					//更新
+	if (test_table_select(test_ret) == 0)					//查询
 	{
 		printf("Select2 test successful \n");
 		print_res(*test_ret);
 		db_free_select_result(test_ret);
 	}
 	
-	if (test_table_delete(&new_example) == 0)			//删除
-	{
-		printf("Delete test successful \n");
-	}
-	if (test_table_select(test_ret) == 0)				//查询
+	test_table_delete(&new_example);					//删除
+	if (test_table_select(test_ret) == 0)					//查询
 	{
 		printf("Select3 test successful \n");
 		print_res(*test_ret);
